af3c84ffc6fa8b6d796fb097833c7bd50fc35ed6
Mark Chmarny <mark@chmarny.com>
2023-12-22 05:43:39 -0800

    release test

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ import (
 
 var (
 	chatter chat.Chat = &gemini.Chat{}
+
+	// set at build time
+	version = "v0.0.1-default"
+	commit  = "not-set"
+	date    = "not-set"
 )
 
 func main() {
@@ -20,15 +25,23 @@ func main() {
 	defer chatter.Close(ctx)
 
 	// flags
+	info := flag.Bool("info", false, "Show version info.")
+
 	if err := chatter.Init(ctx); err != nil {
-		fmt.Printf("Error initializing chat: %s", err.Error())
+		fmt.Printf("error initializing chat: %s", err.Error())
 		return
 	}
 	flag.Parse()
 
+	// info
+	if *info {
+		fmt.Printf("aictl (version: %s, commit: %s, built: %s)\n", version, commit, date)
+		return
+	}
+
 	// prompt
 	if err := chatter.Start(ctx, bufio.NewScanner(os.Stdin)); err != nil {
-		fmt.Printf("Error starting chat: %s", err.Error())
+		fmt.Printf("error starting chat: %s", err.Error())
 		return
 	}
 }
